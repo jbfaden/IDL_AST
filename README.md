1f81bfc2d9e83d36374721da190b2a3083cfc18e
Jeremy Faden <faden@cottagesystems.com>
2025-02-19 17:47:30 -0600

    make into Netbeans (ant) project

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This assumes you are on a Linux machine with antlr4 installed.
 # Instructions
 * git clone git@github.com:jbfaden/IDL_AST.git
 * If antlr is installed: `antlr4 -Dlanguage=Java IDL.g4`, or better:
-* java -jar antlr-4.13.0-complete.jar -Dlanguage=Java IDL.g4 
-* javac -cp .:antlr4-4.13.0-complete.jar *.java
-* java  -cp .:antlr4-4.13.0-complete.jar IDLParserDemo
+* java -jar ../lib/antlr-4.13.2-complete.jar -Dlanguage=Java IDL.g4 
+* javac -cp .:../lib/antlr-4.13.2-complete.jar *.java
+* java  -cp .:../lib/antlr-4.13.2-complete.jar IDLParserDemo
 
---
+++ src/IDLParserDemo.java
@@ -0,0 +1,26 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class IDLParserDemo {
+    public static void main(String[] args) throws Exception {
+        // Sample IDL code
+        String idlCode = """
+            PRINT, x+2*4
+            
+        """;
+
+        // Create input stream
+        CharStream input = CharStreams.fromString(idlCode);
+
+        // Create lexer
+        IDLLexer lexer = new IDLLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // Create parser
+        IDLParser parser = new IDLParser(tokens);
+        ParseTree tree = parser.program();
+
+        // Print AST
+        System.out.println(tree.toStringTree(parser));
+    }
+}

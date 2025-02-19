45573bcbd50aaaa0430ccd7f85d26de5aa015fb4
Jeremy Faden <faden@cottagesystems.com>
2025-02-19 17:58:40 -0600

    allow the first argument to be an IDL script

--- src/IDLParserDemo.java
+++ src/IDLParserDemo.java
@@ -1,13 +1,21 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 public class IDLParserDemo {
     public static void main(String[] args) throws Exception {
-        // Sample IDL code
-        String idlCode = """
-            PRINT, x+2*4
-            
-        """;
+        
+        String idlCode;
+        
+        if ( args.length==0 ) {
+            // Sample IDL code
+            idlCode = """
+                PRINT, x+2*4
+                             """;
+        } else {
+            idlCode= Files.readString(Path.of(args[0]));
+        }
 
         // Create input stream
         CharStream input = CharStreams.fromString(idlCode);
---
+++ src/test.pro
@@ -0,0 +1,3 @@
+x=3
+PRINT, x+2*4
+PRINT, x
